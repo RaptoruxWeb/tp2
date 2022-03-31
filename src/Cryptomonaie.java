d954fb19e994159fd99ad6a4dd545bc30f5318cf
Raptorux <64137980+RaptoruxWeb@users.noreply.github.com>
2022-03-31 10:48:44 +0200

    YOUHOUY COPIE

---
+++ src/Cryptomonaie.java
@@ -0,0 +1,22 @@
+public class Cryptomonaie{
+    private String nom;
+    private double valeurDeJeton; // Imaginons en euros
+
+    public Cryptomonaie(String nom, double valeurDeJeton){
+        this.nom = nom;
+        this.valeurDeJeton = valeurDeJeton;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public double getValeurDeJeton() {
+        return valeurDeJeton;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%5s",nom) + ":" + String.format("%10.1f",valeurDeJeton);
+    }
+}
